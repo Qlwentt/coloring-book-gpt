63111e19f56d1675a08faecce8e26e90fc55a4e2
Qlwentt <quai.wentt@gmail.com>
2024-01-06 16:59:47 -0500

    update hostname

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.hosts << "coloring.book.gpt"
+  config.hosts << "coloring-book-gpt.wl.r.appspot.com"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
