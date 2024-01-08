f1ba20f4c3b51e6ab85a5dde6812c7cf38480ccc
Qlwentt <quai.wentt@gmail.com>
2024-01-07 20:41:45 -0500

    update hosts with new domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,9 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.hosts << "coloring-book-gpt.wl.r.appspot.com"
+  config.hosts << "coloring-book-gpt.com"
+  config.hosts << "www.coloring-book-gpt.com"
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
